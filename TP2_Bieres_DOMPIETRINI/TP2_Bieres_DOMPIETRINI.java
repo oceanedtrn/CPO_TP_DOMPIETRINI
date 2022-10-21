acd56596b05eb7c6fd5fb6c904530d61b4b14859
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-10-21 16:53:26 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_Bieres_DOMPIETRINI/TP2_Bieres_DOMPIETRINI.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_dompietrini;
+
+/**
+ *
+ * @author odomp
+ */
+public class TP2_Bieres_DOMPIETRINI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere("Leffe", 6.6 
+,"Abbaye de Leffe") ;
+        uneBiere.lireEtiquette();
+        System.out.println(uneBiere);
+        BouteilleBiere autreBiere = new BouteilleBiere("Kronenbourg", 3 
+,"Kronenbourg") ;
+        System.out.println(autreBiere);
+        
+        autreBiere.lireEtiquette();
+        BouteilleBiere Biere = new BouteilleBiere("Cuvée des trolls", 
+7.0 ,"Dubuisson") ;
+        Biere.lireEtiquette();
+        System.out.println(Biere);
+        
+        
+        boolean var;
+        var=uneBiere.Decapsuler();
+        System.out.println(var);
+        System.out.println(uneBiere);
+        
+
+    }
+    
+}
+
---
+++ TP2_Bieres_DOMPIETRINI/src/tp2_bieres_dompietrini/BouteilleBiere.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_dompietrini;
+
+/**
+ *
+ * @author odomp
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    public void lireEtiquette() {
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+ " degres) \nBrasserie : " + brasserie ) ;
+
+
+}
+    
+public BouteilleBiere(String unNom, double unDegre, String 
+uneBrasserie) {
+ nom = unNom;
+ degreAlcool = unDegre;
+ brasserie = uneBrasserie;
+ ouverte = false;
+}
+   
+public boolean Decapsuler (){
+    if (ouverte== false){
+        ouverte=true;
+    }
+    else{
+        System.out.println("Erreur, biere deja ouverte");
+        ouverte=false;
+    }
+        return ouverte;
+}
+@Override
+public String toString() {
+String chaine_a_retourner;
+chaine_a_retourner = (nom + " (" + degreAlcool + " degrés \nOuverte ?) "); 
+if (ouverte == true ) chaine_a_retourner += "oui" ;
+else chaine_a_retourner += "non" ;
+return chaine_a_retourner ;
+}
+}
+
+ 
+
+
