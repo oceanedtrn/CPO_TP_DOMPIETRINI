e796b545e1953c832ae0dfc40222e700bd7cd379
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-10-25 10:52:08 +0200

    Version finale, fontionne bien

---
+++ TP2_relation_1_DOMPIETRINI/src/tp2_relation_1_dompietrini/TP2_relation_1_DOMPIETRINI.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_relation_1_dompietrini;
+
+/**
+ *
+ * @author odomp
+ */
+public class TP2_relation_1_DOMPIETRINI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 );
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 );
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 );
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 );
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        System.out.println("liste des voitures disponibles "+ uneClio + 
+        "\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        
+   
+    bob.ajouter_voiture(uneClio);
+    System.out.println ("la premiere voiture de Bob est" + bob.liste_voitures [0]);
+    bob.ajouter_voiture(une2008);
+    System.out.println ("la deuxieme voiture de Bob est" + bob.liste_voitures [1]);
+    reno.ajouter_voiture(uneMicra);
+    System.out.println ("la premiere voiture de Reno est" + reno.liste_voitures [0]);
+    reno.ajouter_voiture(uneMicra);
+    System.out.println ("la deuxieme voiture de Reno est" + reno.liste_voitures [1]);
+
+
+ reno.ajouter_voiture(uneClio); 
+   
+   System.out.println(bob);
+   
+   System.out.println(reno);
+
+}
+}
