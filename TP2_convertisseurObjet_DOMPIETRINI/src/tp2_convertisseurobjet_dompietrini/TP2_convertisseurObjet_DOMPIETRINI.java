8a2063ef69e9c54b08c735b6b8673b576517d005
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-10-25 12:49:39 +0200

    version finale, fonctionne normalement

--- TP2_convertisseurObjet_DOMPIETRINI/src/tp2_convertisseurobjet_dompietrini/TP2_convertisseurObjet_DOMPIETRINI.java
+++ TP2_convertisseurObjet_DOMPIETRINI/src/tp2_convertisseurobjet_dompietrini/TP2_convertisseurObjet_DOMPIETRINI.java
@@ -4,6 +4,8 @@
  */
 package tp2_convertisseurobjet_dompietrini;
 
+import java.util.Scanner;
+
 /**
  *
  * @author odomp
@@ -15,6 +17,53 @@ public class TP2_convertisseurObjet_DOMPIETRINI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    Scanner sc;
+        sc = new Scanner(System.in);
+        
+        convertisseur temperature = new convertisseur();
+        
+        System.out.println("Donnez l'unité de votre température en entrée:\n1 pour Farenheit \n2 pour Celcius\n3 pour Kelvin ");
+        int entrée = sc.nextInt ();
+        
+        System.out.println("Donnez l'unité que vous souhaitez :\n1 pour Farenheit \n2 pour Celcius\n3 pour Kelvin ");
+        int sortie = sc.nextInt ();
+        
+        System.out.println("Donnez la valeur de la température");
+        int c = sc.nextInt ();
+        
+        
+        if (entrée==2 && sortie==3){
+        double result = temperature.CelciusversKelvin (c);
+        System.out.println(result+ " K");
+        }
+       
+        if (entrée==3 && sortie==2){
+        double result1 = temperature.KelvinversCelcius (c);
+        System.out.println(result1+ " °C");
+        }
+        
+        if (entrée==1 && sortie==3){
+        double result2 = temperature.FarenheitversKelvin (c);
+        System.out.println(result2+" K");
+        }
+        
+        if (entrée==3 && sortie==1){
+        double result3 = temperature.KelvinversFarenheit (c);
+        System.out.println(result3+" F");
+        }
+        
+        if (entrée==2 && sortie==1){
+        double result4 = temperature.CelciusversFarenheit (c);
+        System.out.println(result4+" F");
+        }
+        
+        if (entrée==1 && sortie==2){
+        double result5 = temperature.FarenheitversCelcius (c);
+        System.out.println(result5+" °C");
+        }        
     }
-    
 }
+    
+
+    
+
