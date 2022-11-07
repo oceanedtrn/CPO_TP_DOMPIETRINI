884b3b2652e727c8b911c7fd30607157e1d86ba4
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-11-07 11:15:45 +0100

    Interface final

---
+++ ma premiere interface graphique/src/ma/premiere/pkginterface/graphique/textBonjour.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.premiere.pkginterface.graphique;
+
+/**
+ *
+ * @author odomp
+ */
+class textBonjour {
+
+    static void setText(String bouton_cliqué) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
