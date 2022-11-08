022f49ea17b41bf35d25b3dc08f5517ea2cff0a0
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-11-08 15:51:06 +0100

    question 11 faite

--- TP4_OceaneDompietrini/src/tp4_oceanedompietrini/TP4_OceaneDompietrini.java
+++ TP4_OceaneDompietrini/src/tp4_oceanedompietrini/TP4_OceaneDompietrini.java
@@ -14,7 +14,25 @@ public class TP4_OceaneDompietrini {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Epée Epée1 = new Epée( "Excalibur", 7, 5);
+        Epée Epée2=new Epée ( "Durandal", 4, 7);
+        
+        Baton baton1 = new Baton( " Chêne", 4, 5);
+        Baton baton2 = new Baton( " Charme", 5, 6);
+        
+        System.out.println (baton1);
+        System.out.println (Epée2);
+        System.out.println (Epée1);
+        System.out.println (baton2);
+        
+        
+        
+        
+        
+        
+        
+        
+// TODO code application logic here
     }
     
 }
