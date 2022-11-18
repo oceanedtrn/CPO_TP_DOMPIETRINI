f6790807915e392eff4c73ce8681c4c2b3e902d1
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-11-18 11:07:05 +0100

    Création classe PlateauDeJeu

---
+++  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/PlateauDeJeu.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_dompietrini._elziere;
+
+/**
+ *
+ * @author odomp
+ */
+public class PlateauDeJeu { //on crée notre tableau
+    CelluleDeGrille [][]grille = new CelluleDeGrille [6][7];
+    
+    
+}
