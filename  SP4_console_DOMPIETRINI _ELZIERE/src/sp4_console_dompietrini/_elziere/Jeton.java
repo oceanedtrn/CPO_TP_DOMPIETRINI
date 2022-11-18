178d7414b7cf63fa9f8e31e3d7a63c90ae002c96
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-11-18 10:48:07 +0100

    Debut projet avec création class jeton

---
+++  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/Jeton.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_dompietrini._elziere;
+
+/**
+ *
+ * @author odomp
+ */
+public class Jeton {
+    String couleur;
+    
+    private Jeton (String couleurJeton){ // initialise l'attribut couleur du jeton
+        couleur= couleurJeton;
+        
+    }
+    
+    public String lireCouleur(){
+        return couleur;
+    }
+    
+    @Override
+    public String toString(){
+        String chaine_a_retourner;
+        if (couleur == "rouge"){
+           chaine_a_retourner = "R" ;
+        } else {
+            chaine_a_retourner = "J";
+        }
+        return chaine_a_retourner;
+    }
+    
+}
