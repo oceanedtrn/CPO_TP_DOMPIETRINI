2ccffe1cb9804703969cf5bd734f7f91ab41c1c5
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-12-04 21:07:17 +0100

    class partie

---  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/Partie.java
+++  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/Partie.java
@@ -4,6 +4,7 @@
  */
 package sp4_console_dompietrini._elziere;
 import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author odomp
@@ -79,6 +80,107 @@ public class Partie {
         creerEtAffecterJeton(ListeJoueurs[0]);
         creerEtAffecterJeton(ListeJoueurs[1]);
         placerTrousNoirsEtDesintegrateurs();
+        Scanner saisieUtilisateur = new Scanner(System.in);
+        
+        System.out.println("Donner le nom du premier joueur");
+        String nomjoueur1= saisieUtilisateur.next();
+        listeJoueurs[0].donnerNom(nomjoueur1);
+        
+        System.out.println("Donner le nom du deuxieme joueur");
+        String nomjoueur2= saisieUtilisateur.next();
+        listeJoueurs[0].donnerNom(nomjoueur2);
+        
+        Random r= new Random();
+        int joueur= r.nextInt(1);
+        
+        boolean victoire=false;
+        
+        while (victoire != true){ //si personne gagne
+            if (joueur==1){ 
+                joueurCourant = listeJoueurs [0]; //joueur1 qui commence
+                        joueur=0; //changement de joueur qui joue
+            }
+            else 
+                joueurCourant = listeJoueurs[1]; //joueur2 qui commence 
+            joueur=1;
+        }
+        System.out.println ("Que voulez vous faire? \n poser jeton  tapez 1 \n Retirez jeton tapez 2 \n desintegrateur tapez 3 ");
+        int choix= saisieUtilisateur.nextInt();
+        
+        if (choix==1){
+            if (joueurCourant.nombreDeJetons()>0){ //si joueur a jetons encore
+                
+                System.out.println ( "Dans quelle colonne vous voulez poser votre jeton");
+                int colonne= saisieUtilisateur.nextInt();
+                Jeton joué= joueurCourant.jouerJeton();
+                boolean colonnerem= plateau.colonneRemplie(colonne); //check si colonne vide
+                if (colonnerem==false){
+                    plateau.ajouterJetonsDansColonne(joué, colonne); //si colonne vide on pose jeton
+                    
+                    for (int i=1; i<=6; i++ ){
+                        boolean res=plateau.presenceTrouNoir(i, colonne);
+                        if (res== true){
+                            plateau.supprimerTrouNoir(i, colonne);
+                            plateau.supprimerJeton(i, colonne);
+                            
+                        }
+                    }
+                }
+                else {
+                    System.out.println ("Colonne pleine, en choisir une autre");
+                }    
+                
+            }
+            else {
+                System.out.println ("plus de jeton dans votre jeu");
+                
+            }
+            String couleur= joueurCourant.donnerCouleur();
+            victoire = plateau.etreGagnantePourJoueur(joueurCourant);
+        }
+        if (choix ==2){ //retirer un jeton
+            
+            System.out.println ("Dans quelle ligne est le jeton que vous voulez enlever");
+            int ligneretirer= saisieUtilisateur.nextInt();
+            System.out.println ("Dans quelle est le jeton que vous voulez enlever?");
+            int colonneretirer= saisieUtilisateur.nextInt();
+            boolean place = plateau.presenceJeton(ligneretirer, colonneretirer);
+            String couleurjeton= joueurCourant.donnerCouleur();
+            String lirecouleurjeton= plateau.lireCouleurDuJeton(ligneretirer, colonneretirer);
+            if (place==true & couleurjeton== lirecouleurjeton){
+                plateau.recupererJeton(ligneretirer, colonneretirer);
+                plateau.tassercolonne(colonneretirer);
+            }
+            else{
+                System.out.println ("aucun jeton à enlever");
+                
+            }
+            String couleur= joueurCourant.donnerCouleur();
+            victoire= plateau.etreGagnantePourJoueur(couleur);
+            
+        }
+        if (choix==3){
+            System.out.println("ligne du jeton à desintegrer?");
+            int ligneretirer=saisieUtilisateur.nextInt();
+            System.out.println("colonne du jeton à desintegrer?");
+            int colonneretirer=saisieUtilisateur.nextInt();
+            boolean place= plateau.presenceJeton(ligneretirer, colonneretirer);
+            
+            if (place==true){
+                plateau.supprimerJeton(ligneretirer, colonneretirer);
+                plateau.tassercolonne(colonneretirer);
+            }
+            else{
+                System.out.println ("aucun jeton à désintegrer");
+                
+            }
+            joueurCourant.utiliserDesintegrateur();
+            String couleur = joueurCourant.donnerCouleur();
+            victoire = plateau.etreGagnantePourJoueur(couleur);
+        }
     }
+    String nom=joueurCourant.retournerNom();
+    System.out.println(nom + " gagne");
+    
    
 }
