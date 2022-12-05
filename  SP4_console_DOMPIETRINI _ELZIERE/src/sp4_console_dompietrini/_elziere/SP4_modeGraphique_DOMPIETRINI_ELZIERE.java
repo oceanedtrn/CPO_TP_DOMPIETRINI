801f183b6c1a1723f008c66f07b2deaa3b1f3cd1
oceane dompietrini <oceane.dompietrini@epfedu.fr>
2022-12-05 09:32:20 +0100

    interface

---  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/SP4_modeGraphique_DOMPIETRINI_ELZIERE.java
+++  SP4_console_DOMPIETRINI _ELZIERE/src/sp4_console_dompietrini/_elziere/SP4_modeGraphique_DOMPIETRINI_ELZIERE.java
@@ -4,6 +4,9 @@
  */
 package sp4_console_dompietrini._elziere;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author odomp
@@ -16,6 +19,7 @@ PlateauDeJeu plateau;
      * Creates new form SP4_modeGraphique_DOMPIETRINI_ELZIERE
      */
     public SP4_modeGraphique_DOMPIETRINI_ELZIERE() {
+       plateau  = new PlateauDeJeu();
         initComponents();
         panneau_info_joueur.setVisible(false);
         panneau_info_partie.setVisible(false);
@@ -208,6 +212,12 @@ PlateauDeJeu plateau;
        
         panneau_info_joueur.setVisible(true);
         panneau_info_partie.setVisible(true);
+       
+        lancerPartie();
+        
+        panneau_grille.repaint();
+        btn_start.setEnabled(false);
+        
     }//GEN-LAST:event_btn_startActionPerformed
 
     /**
@@ -244,6 +254,183 @@ PlateauDeJeu plateau;
             }
         });
     }
+   
+    public void initialiserPartie() {
+        attribuerCouleurAuxJoueurs();
+        creerEtAffecterJeton(listeJoueurs[0]);
+        creerEtAffecterJeton(listeJoueurs[1]);
+        placerTrousNoirsEtDesintegrateurs();
+    }
+
+     public void lancerPartie(){  
+        
+       
+       
+        String nomjoueur1= nom_Joueur1.getText();
+        Joueur J1 = new Joueur(nomjoueur1);
+        String nomjoueur2= nom_Joueur2.getText();
+        Joueur J2 = new Joueur(nomjoueur2);
+        listeJoueurs[0]= J1;
+        listeJoueurs [1]= J2;
+       
+        
+       attribuerCouleurAuxJoueurs();
+        creerEtAffecterJeton(listeJoueurs[0]);
+        creerEtAffecterJeton(listeJoueurs[1]);
+        placerTrousNoirsEtDesintegrateurs();
+    
+        plateau.afficherGrilleSurConsole();
+
+       System.out.println(J1.afficherNom() + "est de couleur" + J1.couleur);
+       System.out.println(J2.afficherNom() + "est de couleur" + J2.couleur);
+        
+        
+        Random r= new Random();
+        int joueur= r.nextInt(1);
+       
+        boolean victoire=false;
+       /*
+        while (victoire != true){ //si personne gagne
+            if (joueur==1){
+                joueurCourant = listeJoueurs [0]; //joueur1 qui commence
+                        joueur=0; //changement de joueur qui joue
+            }
+            else
+                joueurCourant = listeJoueurs[1]; //joueur2 qui commence
+            joueur=1;
+        }
+        System.out.println ("Que voulez vous faire? \n poser jeton  tapez 1 \n Retirez jeton tapez 2 \n desintegrateur tapez 3 ");
+        int choix= saisieUtilisateur.nextInt();
+       
+        if (choix==1){
+            if (joueurCourant.nombreDeJetons()>0){ //si joueur a jetons encore
+               
+                System.out.println ( "Dans quelle colonne vous voulez poser votre jeton");
+                int colonne= saisieUtilisateur.nextInt();
+                Jeton joué= joueurCourant.jouerJeton();
+                boolean colonnerem= plateau.colonneRemplie(colonne); //check si colonne vide
+                if (colonnerem==false){
+                    plateau.ajouterJetonsDansColonne(joué, colonne); //si colonne vide on pose jeton
+                   
+                    for (int i=1; i<=6; i++ ){
+                        boolean res=plateau.presenceTrouNoir(i, colonne);
+                        if (res== true){
+                            plateau.supprimerTrouNoir(i, colonne);
+                            plateau.supprimerJeton(i, colonne);
+                           
+                        }
+                    }
+                }
+                else {
+                    System.out.println ("Colonne pleine, en choisir une autre");
+                }   
+                
+            }
+            else {
+                System.out.println ("plus de jeton dans votre jeu");
+               
+            }
+            String couleur= joueurCourant.afficherCouleur();
+            victoire = plateau.etreGagnantePourCouleur(couleur);
+            }
+            if (choix ==2){ //retirer un jeton
+           
+                System.out.println ("Dans quelle ligne est le jeton que vous voulez enlever");
+                int ligneretirer= saisieUtilisateur.nextInt();
+                System.out.println ("Dans quelle est le jeton que vous voulez enlever?");
+                int colonneretirer= saisieUtilisateur.nextInt();
+                boolean place = plateau.presenceJeton(ligneretirer, colonneretirer);
+                String couleurjeton= plateau.lireCouleurDuJeton(ligneretirer, colonneretirer);
+                String lirecouleurjeton= plateau.lireCouleurDuJeton(ligneretirer, colonneretirer);
+                
+                if (place==true & couleurjeton== lirecouleurjeton){
+                    plateau.recupererJeton(ligneretirer, colonneretirer);
+                    plateau.tassercolonne(ligneretirer,colonneretirer);
+                }
+                else{
+                    System.out.println ("aucun jeton à enlever");
+               
+            }
+            String couleur= joueurCourant.afficherCouleur();
+            victoire= plateau.etreGagnantePourCouleur(couleur);
+           
+        }
+        if (choix==3){
+            System.out.println("ligne du jeton à desintegrer?");
+            int ligneretirer=saisieUtilisateur.nextInt();
+            System.out.println("colonne du jeton à desintegrer?");
+            int colonneretirer=saisieUtilisateur.nextInt();
+            boolean place= plateau.presenceJeton(ligneretirer, colonneretirer);
+           
+            if (place==true){
+                plateau.supprimerJeton(ligneretirer, colonneretirer);
+                plateau.tassercolonne(ligneretirer,colonneretirer);
+            }
+            else{
+                System.out.println ("aucun jeton à désintegrer");
+               
+            }
+            joueurCourant.utiliserDesintegrateur();
+            String couleur = joueurCourant.afficherCouleur();
+            victoire = plateau.etreGagnantePourCouleur(couleur);
+        }
+    */
+    String nom=joueurCourant.afficherNom();
+    System.out.println(nom + " gagne");
+    }
+   
+     public void  attribuerCouleurAuxJoueurs(){
+        Random r = new Random();
+        int c = r.nextInt(1);
+        if (c == 0){
+            listeJoueurs [0].affecterCouleur("rouge");
+            listeJoueurs [1].affecterCouleur("jaune");
+        }if (c == 1){
+            listeJoueurs [0].affecterCouleur("jaune");
+            listeJoueurs [1].affecterCouleur("rouge");
+        }
+    }
+      public void creerEtAffecterJeton(Joueur joueur) {
+        Jeton[] jetons = new Jeton[30];
+        for (int i = 0; i < 30; i++) {
+            jetons[i] = new Jeton(joueur.couleur);
+            joueur.ajouterJeton(jetons[i]);
+        }
+    }
+
+    public void placerTrousNoirsEtDesintegrateurs() {
+    Random l = new Random();
+        Random c = new Random();
+        for (int i = 0; i < 3; i++) {
+            int ligne = l.nextInt(0, 6);
+            int colonne = c.nextInt(0, 7);
+            if (plateau.presenceTrouNoir(ligne, colonne) == false && plateau.presenceDesintegrateur(ligne, colonne) == false) {
+                plateau.placerTrouNoir(ligne, colonne);
+                plateau.placerDesintegrateur(ligne, colonne);
+            } else {
+                i -= 1;
+            }
+        }
+        for (int j = 0; j < 2; j++) {
+            int ligne = l.nextInt(0, 6);
+            int colonne = c.nextInt(0, 7);
+            if (plateau.presenceDesintegrateur(ligne, colonne) == false) {
+                plateau.placerDesintegrateur(ligne, colonne);
+            } else {
+                j -= 1;
+            }
+        }
+        for (int k = 0; k < 2; k++) {
+            int ligne = l.nextInt(0, 6);
+            int colonne = c.nextInt(0, 7);
+            if (plateau.presenceTrouNoir(ligne, colonne) == false && plateau.presenceDesintegrateur(ligne, colonne) == false) {
+                plateau.placerTrouNoir(ligne, colonne);
+            } else {
+                k -= 1;
+            }
+        }
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btn_col_0;
